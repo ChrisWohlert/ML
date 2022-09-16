9423721f502c9d9492e657b123de7f73d22fd12f
chwo <chwo@bizzkit.com>
2022-09-16 15:39:53 +0200

    Trying diff models

--- Exercises/Kaggle/Housing/ml.py
+++ Exercises/Kaggle/Housing/ml.py
@@ -2,7 +2,8 @@ import os
 import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from datetime import datetime, timedelta
+from datetime import datetime
+from sklearn.tree import DecisionTreeClassifier
 
 
 def find_floor(x):
@@ -26,21 +27,31 @@ dataset = pandas.read_csv(os.path.join(os.getcwd(), "Exercises/Kaggle/Housing/Ho
 
 X = dataset.drop(["Rent"], axis=1)
 y = dataset.take([2], axis=1)
+print(X)
 
 X["Posted On"] = X["Posted On"].apply(lambda x: (datetime.now() - datetime.strptime(x, "%Y-%m-%d")).days)
 floor = X["Floor"].apply(find_floor)
 floors = X["Floor"].apply(find_floors)
 X["Floor"] = floor
 X["Floors"] = floors
+X["Area Type"] = X["Area Type"].factorize()[0]
+X["Area Locality"] = X["Area Locality"].factorize()[0]
+X["City"] = X["City"].factorize()[0]
+X["Furnishing Status"] = X["Furnishing Status"].factorize()[0]
+X["Tenant Preferred"] = X["Tenant Preferred"].factorize()[0]
+X["Point of Contact"] = X["Point of Contact"].factorize()[0]
 
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 print(X_train)
 
-model = LinearRegression()
+#model = LinearRegression()
+model = DecisionTreeClassifier(max_depth=5)
 model.fit(X_train, y_train)
 
-score = model.score(X_test, y_test)
+train_score = model.score(X_train, y_train)
+test_score = model.score(X_test, y_test)
 
-print(score)
+print(train_score)
+print(test_score)
